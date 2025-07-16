6880a6410f620ce327bf32e2b02b37dad105aacb
xmtlzzz <xmtlzloveasuka@gmail.com>
2025-07-16 21:56:42 +0800

    ceshi

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Weekday int
+
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Satday
+)
+
+func echoWeekDay(w Weekday) {
+	fmt.Printf("现在是星期%d\n", w)
+}
+
+var a int = 1
+
+func main() {
+	echoWeekDay(1)
+	echoWeekDay(Monday)
+	echoWeekDay(a)
+}
