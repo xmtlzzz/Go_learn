129054902495ebb1e7d483502094588e5e272472
xmtlzzz <xmtlzloveasuka@gmail.com>
2025-07-22 12:52:03 +0800

    Fibonnacci

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import "fmt"
 
-func main() {
-	a := map[int]int{
-		1: 1,
-		2: 1,
-		3: 2,
-		4: 3,
-		5: 3,
+func fibloop(n int) int {
+	switch {
+	case n < 0:
+		fmt.Println("input int is negtive")
+	case n == 0:
+		return 0
+	case n == 1 || n == 2:
+		return 1
 	}
-	n := 0
-	for v := range a {
-		n += 1
-		fmt.Printf("set的第%d元素为: %d\n", n, v)
+	a, b := 1, 1
+	for i := 0; i < n-2; i++ {
+		a, b = b, a+b
+	}
+	return b
+}
+
+func fib(n int) int {
+	switch {
+	case n < 0:
+		fmt.Println("input int is negtive")
+	case n == 0:
+		return 0
+	case n == 1 || n == 2:
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func fibDG(n, a, b int) int {
+	switch {
+	case n < 0:
+		fmt.Println("input int is negtive")
+	case n == 0:
+		return 0
+	case n == 1 || n == 2:
+		return b
+	}
+	return fibDG(n-1, b, a+b)
+
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		// fmt.Println(fibloop(i))
+		// fmt.Println(fib(i))
+		fmt.Println(fibDG(i, 1, 1))
 	}
 }
